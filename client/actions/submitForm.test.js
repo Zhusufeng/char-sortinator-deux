fb7407a1ec7680c8f78d43d025969bc1ced17576
Lisa Gee <zhusufeng@gmail.com>
2018-01-24 23:25:35 -0800

    Create rudimentary test for submitForm

--- client/actions/submitForm.test.js
+++ client/actions/submitForm.test.js
@@ -1,3 +1,7 @@
-test('two plus two is four', () => {
-  expect(2 + 2).toBe(4);
+const submitForm = require('./submitForm');
+
+test('submitForm works', () => {
+  sortFormInputValue = 'apple';
+  submitForm();
+  expect(sortFormInputValue).toBe('apple');
 });
