d9533043acaf1c1e402dd7e1d645dea05202654c
Lisa Gee <zhusufeng@gmail.com>
2018-01-24 22:40:42 -0800

    Create appendData.view.js to append original & sorted str to index.html

---
+++ client/view/appendData.view.js
@@ -0,0 +1,5 @@
+const appendData = (sortedInput) => {
+  const tr = document.createElement("tr");
+  tr.innerHTML = `<td>${sortFormInputValue}</td><td>${sortedInput}</td>`
+  sortTable.appendChild(tr);
+};
