929a0d4ffd1c54955115b6fa1380788691210f79
Lisa Gee <zhusufeng@gmail.com>
2018-01-24 21:47:18 -0800

    Require sortInput func in server/index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const sortInput = require('./actions/sortInput');
 
 const app = express();
 const port = 8123;
@@ -7,6 +8,9 @@ const host = 'localhost';
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
+
+app.route('/sort')
+  .post(sortInput);
 
 app.listen(port, () => {
   console.log(`Server listening on http://${host}:${port}`);
