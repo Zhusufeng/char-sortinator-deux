b7c27eec8578e9758c83bac935395444f2098e72
Lisa Gee <zhusufeng@gmail.com>
2018-01-25 13:30:28 -0800

    Update appendData.view.js to define sortTable & return tr

--- client/view/appendData.view.js
+++ client/view/appendData.view.js
@@ -1,7 +1,9 @@
 const appendData = (sortedInput) => {
+  const sortTable = document.getElementById('sort-table');
   const tr = document.createElement("tr");
   tr.innerHTML = `<td>${sortFormInputValue}</td><td>${sortedInput}</td>`
   sortTable.appendChild(tr);
+  return tr;
 };
 
 module.exports = appendData;
