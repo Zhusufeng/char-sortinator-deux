3dac2af657509c9172d9674b2d040769dc27041b
Lisa Gee <zhusufeng@gmail.com>
2018-01-24 21:51:51 -0800

    Separate business logic out of sortInput

--- server/actions/sortInput.js
+++ server/actions/sortInput.js
@@ -1,32 +1,9 @@
+const sortDescendingLexicographically = require('./logic/sortDescendingLexicographically');
+
 const sortInput = (req, res) => {
   console.log(req.body.input);
 
-  const sortedString = req.body.input
-    .split(' ')
-    .sort((a, b) => {
-      if(a.charCodeAt(0) > b.charCodeAt(0)) {
-        return -1;
-      } else if (a.charCodeAt(0) === b.charCodeAt(0)) {
-        let len;
-        if (a.length > b.length) {
-          len = b.length;
-        } else {
-          len = a.length;
-        }
-        for (let i = 1; i < len; i++) {
-          if (a.charCodeAt(i) > b.charCodeAt(i)) {
-            return -1;
-          } else if (a.charCodeAt(i) === b.charCodeAt(i)) {
-            continue;
-          } else {
-            return 1;
-          }
-        }
-      } else {
-        return 1;
-      }
-    })
-    .join(' ');
+  const sortedString = sortDescendingLexicographically(req.body.input);
 
   res.status(201).send(JSON.stringify({sortedString}));
 };
