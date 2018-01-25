b73c1f15d54e27339ee6f6365faeec7f97184773
Lisa Gee <zhusufeng@gmail.com>
2018-01-24 23:34:24 -0800

    Add tests to sortDescLexico

--- server/logic/sortDescendingLexicographically.test.js
+++ server/logic/sortDescendingLexicographically.test.js
@@ -2,4 +2,7 @@ const sortDescLexico = require('./sortDescendingLexicographically');
 
 test('sortDescendingLexicographically works', () => {
   expect(sortDescLexico('apple bear')).toBe('bear apple');
+  expect(sortDescLexico('super duper')).toBe('super duper');
+  expect(sortDescLexico('cat cake chat')).toBe('chat cat cake');
+  expect(sortDescLexico('a aa aaa aaaa1 aaaa2')).toBe('aaaa2 aaaa1 aaa aa a');
 });
