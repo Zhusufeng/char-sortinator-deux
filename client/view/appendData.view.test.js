f831ac5141bb7ba950fd105151259fc3621782c5
Lisa Gee <zhusufeng@gmail.com>
2018-01-25 13:30:58 -0800

    Adjust test for appendData.view.test.js to make testable

--- client/view/appendData.view.test.js
+++ client/view/appendData.view.test.js
@@ -1,5 +1,7 @@
-const appendData = require('./appendData');
+const appendData = require('./appendData.view');
 
 test('appendData works', () => {
-  
+  const sortedInput1 = 'cat bear apple';
+  const sortFormInputValue1 = 'apple bear cat';
+  expect(appendData(sortedInput1, sortFormInputValue1).innerHTML).toBe(`<td>${sortFormInputValue}</td><td>${sortedInput}</td>`);
 });
