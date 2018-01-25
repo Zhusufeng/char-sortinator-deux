e94ac735c97e0970940225787932f5497133c683
Lisa Gee <zhusufeng@gmail.com>
2018-01-24 22:26:08 -0800

    Factor out action, function submitForm, from index.view.js

--- client/view/index.view.js
+++ client/view/index.view.js
@@ -1,12 +1,4 @@
 const sortForm = document.getElementById('sort-form');
 const sortFormInput = document.getElementById('sort-form-input');
 
-const submitForm = (e) => {
-  e.preventDefault();
-  console.log('submitForm was pushed');
-
-  postString(sortFormInput.value);
-  sortForm.reset();
-};
-
 sortForm.addEventListener('submit', submitForm);
