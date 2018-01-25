79ebf86b1d314716df35971317a220b965b0ab2d
Lisa Gee <zhusufeng@gmail.com>
2018-01-24 22:23:39 -0800

    Update postString to take in input

--- client/actions/postString.js
+++ client/actions/postString.js
@@ -1,6 +1,8 @@
-const postString = () => {
-  const testObj = {
-    input: 'apple bear cat chat cake dear'
+const postString = (input) => {
+  const string = {
+    // input: 'aa aaa aaaa1 aaaa2 bear cat chat cake dear eat1 eat2 a apple'
+    // input: 'a aa aaa aaaa1 aaaa2'
+    input
   };
 
   fetch('/sort', {
@@ -8,7 +10,7 @@ const postString = () => {
     headers: new Headers({
       'Content-Type': 'application/json'
     }),
-    body: JSON.stringify(testObj)
+    body: JSON.stringify(string)
   })
   .then(data => {
     return data.json();
