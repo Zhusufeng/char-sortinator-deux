ea6e3169f9d049e678138fbfdf419e1234a3feeb
Lisa Gee <zhusufeng@gmail.com>
2018-01-24 21:53:37 -0800

    Tidy up code in sortInput, remove logs, etc

--- server/actions/sortInput.js
+++ server/actions/sortInput.js
@@ -1,10 +1,7 @@
-const sortDescendingLexicographically = require('./logic/sortDescendingLexicographically');
+const sortDescendingLexicographically = require('../logic/sortDescendingLexicographically');
 
 const sortInput = (req, res) => {
-  console.log(req.body.input);
-
   const sortedString = sortDescendingLexicographically(req.body.input);
-
   res.status(201).send(JSON.stringify({sortedString}));
 };
 
