4241d628c6c45c78e18710d46f3710c5ddbde011
Lisa Gee <zhusufeng@gmail.com>
2018-01-24 22:14:55 -0800

    Fix up the customSort in sortDescendingLexicographically

--- server/logic/sortDescendingLexicographically.js
+++ server/logic/sortDescendingLexicographically.js
@@ -1,30 +1,39 @@
 const sortDescendingLexicographically = (input) => {
   return input
     .split(' ')
-    .sort((a, b) => {
-      if(a.charCodeAt(0) > b.charCodeAt(0)) {
+    .sort(customSort)
+    .join(' ');
+};
+
+const customSort = (a, b) => {
+  console.log(a, b);
+  if(a.charCodeAt(0) > b.charCodeAt(0)) {
+    return -1;
+  } else if (a.charCodeAt(0) === b.charCodeAt(0)) {
+    let len;
+    if (a.length < b.length) { // 'a' comes before 'aaa'
+      len = b.length;
+    } else {
+      len = a.length;
+    }
+    console.log(len);
+    for (let i = 1; i < len; i++) {
+      if (a.charCodeAt(i) > b.charCodeAt(i)) {
         return -1;
-      } else if (a.charCodeAt(0) === b.charCodeAt(0)) {
-        let len;
-        if (a.length > b.length) {
-          len = b.length;
-        } else {
-          len = a.length;
+      } else if (a.charCodeAt(i) === b.charCodeAt(i)) {
+        if (!a[i + 1]) { // 'aa' comes before 'aaa'
+          return 1;
+        } else if (!b[i + 1]) {
+          return -1;
         }
-        for (let i = 1; i < len; i++) {
-          if (a.charCodeAt(i) > b.charCodeAt(i)) {
-            return -1;
-          } else if (a.charCodeAt(i) === b.charCodeAt(i)) {
-            continue;
-          } else {
-            return 1;
-          }
-        }
+        continue;
       } else {
         return 1;
       }
-    })
-    .join(' ');
+    }
+  } else {
+    return 1;
+  }
 };
 
 module.exports = sortDescendingLexicographically;
