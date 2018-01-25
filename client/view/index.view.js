39439d494313da738ce77e9bfc3a8b6a71f995bf
Lisa Gee <zhusufeng@gmail.com>
2018-01-25 13:31:29 -0800

    Remove sortTable selector from index.view.js

--- client/view/index.view.js
+++ client/view/index.view.js
@@ -1,6 +1,6 @@
 const sortForm = document.getElementById('sort-form');
 const sortFormInput = document.getElementById('sort-form-input');
-const sortTable = document.getElementById('sort-table');
+
 let sortFormInputValue = '';
 
 sortForm.addEventListener('submit', submitForm);
