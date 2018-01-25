d0468c46b83df1085f96e734b81a7f8ce62e5918
Lisa Gee <zhusufeng@gmail.com>
2018-01-24 23:37:45 -0800

    Separate tests in sortDescLexico test file to specify differences

--- server/logic/sortDescendingLexicographically.test.js
+++ server/logic/sortDescendingLexicographically.test.js
@@ -1,8 +1,17 @@
 const sortDescLexico = require('./sortDescendingLexicographically');
 
-test('sortDescendingLexicographically works', () => {
+test('sortDescendingLexicographically works - turn ascending to descending order', () => {
   expect(sortDescLexico('apple bear')).toBe('bear apple');
+});
+
+test('sortDescendingLexicographically works - if already in descending order, keep descending order', () => {
   expect(sortDescLexico('super duper')).toBe('super duper');
+});
+
+test('sortDescendingLexicographically works - if multiple words start with the same letter', () => {
   expect(sortDescLexico('cat cake chat')).toBe('chat cat cake');
+});
+
+test('sortDescendingLexicographically works - if multiple words start with the same letter but differ in length', () => {
   expect(sortDescLexico('a aa aaa aaaa1 aaaa2')).toBe('aaaa2 aaaa1 aaa aa a');
 });
