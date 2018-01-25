c94a263b1fcce2e57152ad4f356219d2bf1c9abd
Lisa Gee <zhusufeng@gmail.com>
2018-01-24 19:05:32 -0800

    Add app.listen to server/index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const port = 8123;
+const host = 'localhost';
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + 'client/public'));
+
+app.listen(port, () => {
+  console.log(`Server listening on http://${host}:${port}`);
+});
 
 module.exports = app;
